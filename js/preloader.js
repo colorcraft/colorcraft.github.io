29fc7cdce3c92b5830ed9e6684158cacd01fc24d
Joseph Taylor <josphtaylor@gmail.com>
2015-05-26 17:22:54 -0400

    Retina SrcSet Images Initial

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,7 @@
 jQuery(window).load(function() { 
 "use strict";
-			jQuery("#status").fadeOut(350); // will first fade out the loading animation
-			jQuery("#preloader").delay(350).fadeOut(200); // will fade out the white DIV that covers the website.
+			jQuery("#status").fadeOut(550); // will first fade out the loading animation
+			jQuery("#preloader").delay(550).fadeOut(400); // will fade out the white DIV that covers the website.
 		});
 
 	
